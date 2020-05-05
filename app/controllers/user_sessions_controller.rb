e66d18c3392c9a77172b05f8a9b4e17072c57750
hiro266 <t1freejob@gmail.com>
2020-05-05 12:52:37 +0900

    [fix]ユーザーログインに不要なアクションを削除

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,17 +1,7 @@
 class UserSessionsController < ApplicationController
-  skip_before_action :require_login, only: %i[new create guest_login]
+  skip_before_action :require_login, only: %i[new guest_login]
 
   def new; end
-
-  def create
-    @user = login(params[:email], params[:password])
-    if @user
-      redirect_back_or_to(root_url, success: t('.flash.login'))
-    else
-      flash.now[:danger] = t('.flash.login_failure')
-      render :new
-    end
-  end
 
   def destroy
     logout
