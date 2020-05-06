53cdde5e67d2f78de71e48f848e8d44f94069f67
hiro266 <t1freejob@gmail.com>
2020-05-06 13:45:27 +0900

    [add]rails generateで自動生成されるファイルの設定

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,16 @@ module SoccerPlayerSpikes
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # rails gコマンド使用時に余計なファイルを生成させない
+    config.generators do |g|
+      g.assets false
+      g.skip_routes false
+      g.test_framework :rspec,    # RSpecを使用
+        controller_specs: false,  # controller specは作らない
+        view_specs: false,        # view specは作らない
+        helper_specs: false,      # helper specは作らない
+        routing_specs: false      # routing specは作らない
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
