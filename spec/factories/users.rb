13cc973c296b17e9c033aac982e02312cf98533b
hiro266 <t1freejob@gmail.com>
2020-05-06 09:32:33 +0900

    [add]userのFactoryを作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,32 @@
+# FactoryBot.define do
+#   factory :user do
+#     screen_name { "MyString" }
+#     twitter_email { "MyString" }
+#     twitter_image { "MyString" }
+#     role { 1 }
+#     notification { 1 }
+#   end
+# end
+
 FactoryBot.define do
   factory :user do
-    twiiter_name { "MyString" }
-    twitter_email { "MyString" }
-    twitter_image { "MyString" }
-    role { 1 }
-    notification { 1 }
+    sequence(:twitter_id) { |n| n }
+    sequence(:email) { |n| "person#{n}@example.com" }
+    notification { 0 }
+
+    trait :twitter do
+      screen_name { 'ツイッターユーザー' }
+      role { 0 }
+    end
+
+    trait :admin do
+      screen_name { '管理者' }
+      role { 1 }
+    end
+
+    trait :guest do
+      screen_name { 'ゲストユーザー' }
+      role { 2 }
+    end
   end
 end
