65df87fc211b4173c64ea9b72d0cec0001e5f321
hiro266 <t1freejob@gmail.com>
2020-05-06 18:35:08 +0900

    [add]system spec作成

---
+++ spec/system/login_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Login', type: :system do
+  let(:guest_user) { create(:user, :guest) }
+  before { login(guest_user) }
+  describe 'ログインページ' do
+    it 'ログインボタンが存在すること' do
+      visit login_path
+      expect(page).to have_content('ゲストログイン'), '「ゲストログイン」というテキストを持ったボタンが存在しません'
+      expect(page).to have_content('ツイッターログイン'), '「ツイッターログイン」というテキストを持ったボタンが存在しません'
+    end
+
+    fit 'ゲストログインできること' do
+      expect(current_path).to eq root_path
+      expect(page).to have_content('ゲストユーザーでログインしました'), '「ゲストユーザーでログインしました」というメッセージが存在しません'
+    end
+  end
+end
