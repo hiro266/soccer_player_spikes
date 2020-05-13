bbe848ddfc419421e84c083563a17b9b9ec2677a
hiro266 <t1freejob@gmail.com>
2020-05-13 19:09:43 +0900

    [add]RequestsSpec ステータスコード確認

--- spec/requests/admin/dashboards_request_spec.rb
+++ spec/requests/admin/dashboards_request_spec.rb
@@ -1,12 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe "Admin::Dashboards", type: :request do
+RSpec.describe "ステータスコード確認 Admin::Dashboards", type: :request do
+  let(:admin_user) { create(:user, :admin) }
+  let(:guest_user) { create(:user, :guest) }
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/admin/dashboards/index"
-      expect(response).to have_http_status(:success)
+  describe "GET /index 管理ダッシュボード" do
+    it "管理者は管理画面へのアクセス時にステータス200を返す" do
+      get admin_login_path(admin_user)
+      get admin_root_path
+      expect(response).to have_http_status(200)
     end
+
+    it "ゲストユーザーは管理画面へのアクセス時にステータス404を返す" do
+      get login_path(guest_user)
+      get admin_root_path
+      expect(response).to have_http_status(404)
+    end
+
+    # it 'ツイッターユーザーは管理画面へのアクセス時にステータス404を返す' do
+    # end
   end
 
 end
