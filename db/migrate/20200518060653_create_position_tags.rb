dbc30d8a8033dc50b4ca1a15ee552aa5e94ded26
hiro266 <t1freejob@gmail.com>
2020-05-22 02:22:19 +0900

    add: nil制約

--- db/migrate/20200518060653_create_position_tags.rb
+++ db/migrate/20200518060653_create_position_tags.rb
@@ -1,10 +1,9 @@
 class CreatePositionTags < ActiveRecord::Migration[5.2]
   def change
     create_table :position_tags do |t|
-      t.string :position
+      t.string :position, null: false, unique: true
 
       t.timestamps
     end
-    add_index :position_tags, :position, unique: true
   end
 end
