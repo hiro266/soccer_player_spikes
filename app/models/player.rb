033af15c835fead2fe0ea55173c8d1af6fe021a2
hiro266 <t1freejob@gmail.com>
2020-05-22 20:10:24 +0900

    add: STI設計に変更

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,12 +2,11 @@ class Player < ApplicationRecord
   include Common_Processing
   mount_uploader :image, PlayerUploader
   belongs_to :spike
-  has_many :player_position_tags, dependent: :destroy
-  has_many :player_genre_tags, dependent: :destroy
-  has_many :player_strength_tags, dependent: :destroy
-  has_many :position_tags, through: :player_position_tags
-  has_many :genre_tags, through: :player_genre_tags
-  has_many :strength_tags, through: :player_strength_tags
+  has_many :player_tags, dependent: :destroy
+  has_many :tags, through: :player_tags
+  has_many :position_tags, through: :player_tags
+  has_many :genre_tags, through: :player_tags
+  has_many :strength_tags, through: :player_tags
 
   validates :name,
             :image,
@@ -15,10 +14,10 @@ class Player < ApplicationRecord
 
   def save_tags_for_player(position_tag_names, genre_tag_names, strength_tag_names)
     # ----ポジションタグの処理
-    self.position_tags = position_tag_names.split(',').map { |name| PositionTag.find_or_create_by(position: name) }
+    self.position_tags = position_tag_names.split(',').map { |name| PositionTag.find_or_create_by(name: name) }
     #----ジャンルタグの処理
-    self.genre_tags = genre_tag_names.split(',').map { |name| GenreTag.find_or_create_by(genre: name) }
+    self.genre_tags = genre_tag_names.split(',').map { |name| GenreTag.find_or_create_by(name: name) }
     #----タイプタグの処理
-    self.strength_tags = strength_tag_names.split(',').map { |name| StrengthTag.find_or_create_by(strength: name) }
+    self.strength_tags = strength_tag_names.split(',').map { |name| StrengthTag.find_or_create_by(name: name) }
   end
 end
