4c9dc27fe5703a28410191778c324160cfd5fb57
hiro266 <t1freejob@gmail.com>
2020-05-22 20:11:35 +0900

    add: spec一旦commit

---
+++ spec/factories/player_tags.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :player_tag do
+    player { nil }
+    tag { nil }
+  end
+end
---
+++ spec/factories/tags.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :tag do
+    type { "" }
+    name { "MyString" }
+  end
+end
