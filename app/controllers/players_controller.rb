7b09481f85fb51c416063f1dac2bf4e764388baf
hiro266 <t1freejob@gmail.com>
2020-05-22 20:21:46 +0900

    merge コンフリクト解消

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,11 @@
 class PlayersController < ApplicationController
-  skip_before_action :require_login, only: [:index]
+  skip_before_action :require_login, only: %i[index show]
+
   def index
+    @q = Player.ransack(params[:q]) # 検索オブジェクト作成
+    @players = @q.result.new_order.page(params[:page]) # 検索結果(何もなければ全件取得)
+  end
+
+  def show
   end
 end
