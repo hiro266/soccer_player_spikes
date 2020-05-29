a0de49d4c35d5f1a8e30edc19537f861a699ab10
hiro266 <t1freejob@gmail.com>
2020-05-30 04:07:33 +0900

    add: タブの切り替えをJSで制御

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,18 @@
 //= require activestorage
 //= require swiper/js/swiper.js
 //= require swiper.js
+
+// タブ
+$(function () {
+    // ①タブをクリックしたら発動
+  $('.tab li').click(function () {
+    // ②クリックされたタブの数字を変数に格納
+    var index = $('.tab li').index(this);
+    // ③クリック済みタブのデザインを設定したcssのクラスを一旦削除
+    $('.tab li').removeClass('active');
+    // ④クリックされたタブにクリック済みデザインを適用する
+    $(this).addClass('active');
+    // ⑤コンテンツを一旦非表示にし、クリックされた順番のコンテンツのみを表示
+    $('.players-area > .container').removeClass('show').eq(index).addClass('show');
+  });
+});
