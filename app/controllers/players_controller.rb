cb5c7b91248ea36688581642a057bd88131e7eb7
hiro266 <t1freejob@gmail.com>
2020-06-19 11:28:45 +0900

    modify: ページネーションのurlを変更

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,8 +3,8 @@ class PlayersController < ApplicationController
 
   def index
     @q = Player.ransack(params[:q]) # 検索オブジェクト作成
-    @all_japan_players = @q.result.genre_tag_is_all_japan?.name_asc.page(params[:all_japan])
-    @olympic_japan_players = @q.result.genre_tag_is_olympic_japan?.name_asc.page(params[:olympic_japan])
-    @baron_d_or_players = @q.result.genre_tag_is_baron_d_or?.name_asc.page(params[:baron_d_or])
+    @all_japan_players = @q.result.genre_tag_is_all_japan?.name_asc.page(params[:all_japan_page])
+    @olympic_japan_players = @q.result.genre_tag_is_olympic_japan?.name_asc.page(params[:olympic_japan_page])
+    @baron_d_or_players = @q.result.genre_tag_is_baron_d_or?.name_asc.page(params[:baron_d_or_page])
   end
 end
